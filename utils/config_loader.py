6e2f7d06c1eb8f9ee23dbb21540c1773a25fa0f9
Mandeep Singh <mandeep14042002@gmail.com>
2026-02-14 01:18:17 +0530

    model_loader_utils_added

---
+++ utils/config_loader.py
@@ -0,0 +1,8 @@
+import yaml 
+import os
+
+def load_config(config_path: str = "config/config.yaml") -> dict:
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+        print(f"Config loaded from {config_path}: {config}")
+    return config
